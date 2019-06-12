986a499c8f3115a23c027a692abc68542b9b441b
Clayder.Huang <clayder.huang@advantech.com.tw>
2019-06-12 23:53:35 +0800

    upload mk-adv script

---
+++ mk-adv.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+TARGET_ROOTFS_DIR="binary"
+
+echo "In mk-adv.sh"
+
+echo "Install/remove/adjust debian"
+
+cat <<EOF | sudo chroot $TARGET_ROOTFS_DIR
+apt-get update
+
+apt-get install -y libssl1.0-dev
+apt-get install -y libxml2-dev
+apt-get install -y libxtst-dev
+
+apt-get install -y zlib1g-dev
+apt-get install -y libmosquitto-dev
+apt-get install -y libcurl4-openssl-dev
+apt-get install -y build-essential
+
+apt-get install -y autoconf automake libtool
+
+apt-get install -y cmake
+EOF
+
