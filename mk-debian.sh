8d919248e2270a3eae4e8160a0728d7d9db7863d
YunJin.Jiang <yunjin.jiang@advantech.com.cn>
2020-04-01 16:13:21 +0800

    add debian base for rb04

---
+++ mk-debian.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+TOP_DIR=`pwd`
+RK_DEBIAN="rootfs"
+ADV_DEBIAN="rootfs_adv"
+echo Top of tree: ${TOP_DIR}
+
+# make debian base
+if [ "$1" == "new" ]; then
+    echo "make debian base new"
+    sudo ARCH=armhf RELEASE=stretch ./mk-base-debian.sh
+else
+    echo "use exist debian base"
+    cat $ADV_DEBIAN/debian-base/linaro-stretch-alip-*.tar.gz* > $RK_DEBIAN/linaro-stretch-alip-whole.tar.gz
+fi
+
+# make rockchip
+cd $TOP_DIR/$RK_DEBIAN
+sudo ARCH=armhf ./mk-rootfs.sh
+
+# make adv
+cd $TOP_DIR/$ADV_DEBIAN
+sudo ARCH=armhf ./mk-adv.sh
+
+# mk-image
+cd $TOP_DIR/$RK_DEBIAN
+sudo ./mk-image.sh
+
