867b2fdbc1418f97cf01a3d0996f9324fb528fa6
advyunjin <yunjin.jiang@advantech.com.cn>
2020-09-09 17:42:16 +0800

    update adv_update_ota shell

--- ubuntu16.04/overlay-chuanda/usr/bin/adv_update_ota.sh
+++ ubuntu16.04/overlay-chuanda/usr/bin/adv_update_ota.sh
@@ -8,39 +8,36 @@
 ###########################################################################
 
 ##-------------------------------------------------------------------------
-## The following parameters should be modified according customer's FTP configuration.
-## 
-FTP_SERVER_IP=172.21.170.48
-FTP_USER=yunjin
-FTP_PASSWORD=jiang001
-FTP_PATH="code/rk_update/rsb4710"
-FTP_CONFIG_FILE="/etc/ftp_config.ini"
-##-------------------------------------------------------------------------
 OTG_PATH="/oem/update_ota"
-LOG_FILE=$OTG_PATH/update.log
+FTP_CONFIG_FILE="/etc/ftp_ota.conf"
+FTP_DOWNLOAD_LOG=ftp_update_download_image.log
+
+FTP_DOWNLOAD_TRY_COUNT=2
+FTP_DOWNLOAD_TRY_DELAY=60
+
 WAIT_TIME=10
 TRY_COUNT=10
 ##-------------------------------------------------------------------------
-## get FTP configuration from /etc/ftp_config.ini.
+## get FTP configuration from FTP_CONFIG_FILE.
 ##-------------------------------------------------------------------------
 
-get_ftp_configuration()
+function get_ftp_configuration()
 {
     FTP_SERVER_IP=`grep ftpserver_ip $FTP_CONFIG_FILE | grep -n '' | grep "${line_num}:" | cut -d= -f2 | awk '{print $1}'`
     FTP_USER=`grep ftpserver_user $FTP_CONFIG_FILE | grep -n '' | grep "${line_num}:" | cut -d= -f2 | awk '{print $1}'`
     FTP_PASSWORD=`grep ftpserver_passwd $FTP_CONFIG_FILE | grep -n '' | grep "${line_num}:" | cut -d= -f2 | awk '{print $1}'`
     FTP_PATH=`grep ftpserver_dirname $FTP_CONFIG_FILE | grep -n '' | grep "${line_num}:" | cut -d= -f2 | awk '{print $1}'`
-	
-	echo "FTP_SERVER_IP : $FTP_SERVER_IP"
-	echo "FTP_USER      : $FTP_USER"
-	echo "FTP_PASSWORD  : $FTP_PASSWORD"
-	echo "FTP_PATH      : $FTP_PATH"		
+
+    echo "FTP_SERVER_IP : $FTP_SERVER_IP"
+    echo "FTP_USER      : $FTP_USER"
+    echo "FTP_PASSWORD  : $FTP_PASSWORD"
+    echo "FTP_PATH      : $FTP_PATH"
 }
 
 function ftp_get_version()
 {
     cd $OTG_PATH
-    ftp -v -n ${FTP_SERVER_IP} <<-EOF
+    pftp -v -n ${FTP_SERVER_IP} <<-EOF
         user "${FTP_USER}" "${FTP_PASSWORD}"
         prompt
         binary
@@ -55,17 +52,49 @@ function ftp_get_version()
 function ftp_get_image()
 {
     cd /userdata
-    pftp -v -n ${FTP_SERVER_IP} <<-EOF
-        user "${FTP_USER}" "${FTP_PASSWORD}"
-        prompt
-        binary
-        pwd
-        cd $FTP_PATH
-        mget update.img
-        mget update.img.md5
-        close
-        quit
-	EOF
+    try_time=$FTP_DOWNLOAD_TRY_COUNT
+
+    if [ -f "$FTP_DOWNLOAD_LOG" ]; then
+        rm $FTP_DOWNLOAD_LOG
+    fi
+
+    while [ $try_time -gt 0 ]
+    do
+        pftp -v -n ${FTP_SERVER_IP} >$FTP_DOWNLOAD_LOG <<-EOF
+            user "${FTP_USER}" "${FTP_PASSWORD}"
+            prompt
+            binary
+            pwd
+            cd $FTP_PATH
+            mget update.img.md5
+            mget update.img
+            close
+            quit
+		EOF
+
+        ftp_status=`grep "Transfer complete" $FTP_DOWNLOAD_LOG -ic`
+        if [ "$ftp_status" == "2" ];then
+            echo "Ftp download update.img sucessful"
+            break
+        else
+            # clean error image
+            if [ -f "/userdata/update.img" ]; then
+                rm /userdata/update.img
+            fi
+
+            if [ -f "/userdata/update.img.md5" ]; then
+                rm /userdata/update.img.md5
+            fi
+            echo "Ftp download update.img Fail, try time left : $try_time"
+        fi
+
+        sleep $FTP_DOWNLOAD_TRY_DELAY
+        let try_time--
+    done
+
+    if [ -f "$FTP_DOWNLOAD_LOG" ]; then
+        rm $FTP_DOWNLOAD_LOG
+    fi
     sync
 }
 
@@ -79,34 +108,52 @@ function ftp_get_update_shell()
         binary
         pwd
         cd $FTP_PATH
-        mget 00_update_before.sh
-        mget 01_update_after.sh
+        mget PreUpdate.sh
+        mget PostUpdate.sh
         close
         quit
 	EOF
 
     sync
-	chmod +x 00_update_before.sh
-    chmod +x 01_update_after.sh
+    chmod +x PreUpdate.sh
+    chmod +x PostUpdate.sh
 }
 
-function udisk_get_image
+function udisk_get_image()
 {
     cd /userdata
-    cp /media/root/UPDATE/update.img ./
     cp /media/root/UPDATE/update.img.md5 ./
+    cp /media/root/UPDATE/update.img ./
     sync
 }
 
-function udisk_get_update_shell
+function udisk_get_update_shell()
 {
     cd $OTG_PATH
 
-    cp /media/root/UPDATE/00_update_before.sh ./
-    cp /media/root/UPDATE/01_update_after.sh ./
-	sync
-	chmod +x 00_update_before.sh
-    chmod +x 01_update_after.sh
+    cp /media/root/UPDATE/PreUpdate.sh ./
+    cp /media/root/UPDATE/PostUpdate.sh ./
+    sync
+    chmod +x PreUpdate.sh
+    chmod +x PostUpdate.sh
+}
+
+function update_clean()
+{
+    echo "Make clean for updating..."
+    if [ -f "/userdata/update.img" ]; then
+        rm /userdata/update.img
+    fi
+
+    if [ -f "/userdata/update.img.md5" ]; then
+        rm /userdata/update.img.md5
+    fi
+
+    if [ ! -d "$OTG_PATH" ]; then
+        mkdir -p $OTG_PATH
+    fi
+    rm -rf $OTG_PATH/*
+    sync
 }
 
 # --------------------------------------------------------- #
@@ -114,10 +161,11 @@ function udisk_get_update_shell
 if [ -f "$OTG_PATH/update_flag" ]; then
     echo "Exist updat flag"
     rm $OTG_PATH/update_flag
-	if [ -f "$OTG_PATH/01_update_after.sh" ]; then
-        echo "Exec 01_update_after.sh"
-        $OTG_PATH/01_update_after.sh
-	fi
+    if [ -f "$OTG_PATH/PostUpdate.sh" ]; then
+        echo "Exec PostUpdate.sh"
+        $OTG_PATH/PostUpdate.sh
+        rm $OTG_PATH/PostUpdate.sh
+    fi
     exit 0
 fi
 
@@ -130,22 +178,15 @@ fi
 echo "Update from $UPDATE_RESOURCE"
 
 # clean 
-echo "Make clean for updating..."
+update_clean
 
-if [ -f "/userdata/update.img" ]; then
-    rm /userdata/update.img
+# check for FTP configuration
+if [ "$UPDATE_RESOURCE" == "FTP" ]; then
+    if [ ! -f "$FTP_CONFIG_FILE" ]; then
+        echo "Don't exist FTP configuration file $FTP_CONFIG_FILE"
+        exit 0
+    fi
 fi
-
-if [ -f "/userdata/update.img.md5" ]; then
-    rm /userdata/update.img.md5
-fi
-
-if [ -d "$OTG_PATH" ]; then
-    rm -rf $OTG_PATH
-fi
-mkdir -p $OTG_PATH
-
-sync
 
 #wait time
 sleep $WAIT_TIME
@@ -162,7 +203,7 @@ if [ "$UPDATE_RESOURCE" == "UDISK" ]; then
             sleep 1
         else
             cp -rf /media/root/UPDATE/version $OTG_PATH/version
-			break
+            break
         fi
     done
 else
@@ -180,15 +221,17 @@ else
 fi
 
 if [ ! -f "$OTG_PATH/version" ]; then
-	echo "No version file in $UPDATE_RESOURCE, Update abort"
-	exit 0
+    echo "No version file in $UPDATE_RESOURCE, Update abort"
+    update_clean
+    exit 0
 fi
-		
+
 version_des=`cat $OTG_PATH/version`
 rm $OTG_PATH/version
 if [ "$version_src" == "$version_des" ]; then
-	echo "Version is the same, Already updated!"
-	exit 0
+    echo "Version is the same, Already updated!"
+    update_clean
+    exit 0
 fi
 
 # get update image
@@ -203,11 +246,13 @@ fi
 # check update image
 if [ ! -f "/userdata/update.img" ]; then
     echo "Not found update.img"
+    update_clean
     exit 0
 fi
 
 if [ ! -f "/userdata/update.img.md5" ]; then
     echo "Not found update.img.md5"
+    update_clean
     exit 0
 fi
 
@@ -216,6 +261,7 @@ md5src=`cat /userdata/update.img.md5 | awk '{print $1}'`
 md5des=`md5sum /userdata/update.img | awk '{print $1}'`
 if [ "$md5src" != "$md5des" ]; then
     echo "update.img: md5 value check fail"
+    update_clean
     exit 0
 fi
 
@@ -223,10 +269,10 @@ rm /userdata/update.img.md5
 sync
 
 # before update shell
-if [ -f "$OTG_PATH/00_update_before.sh" ]; then
-    echo "Exec 00_update_before.sh"
-    $OTG_PATH/00_update_before.sh
-    rm $OTG_PATH/00_update_before.sh
+if [ -f "$OTG_PATH/PreUpdate.sh" ]; then
+    echo "Exec PreUpdate.sh"
+    $OTG_PATH/PreUpdate.sh
+    rm $OTG_PATH/PreUpdate.sh
 fi
 
 # update image
