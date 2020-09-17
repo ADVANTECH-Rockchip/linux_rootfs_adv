81ce775cdcf46548abdeda546407ffbece1d8976
advyunjin <yunjin.jiang@advantech.com.cn>
2020-09-17 13:27:03 +0800

    support wifi modle:  EWM-W188M201E and AW-CM276MA for chuanda

--- ubuntu16.04/mk-chuanda-v2.sh
+++ ubuntu16.04/mk-chuanda-v2.sh
@@ -3,6 +3,12 @@ TARGET_ROOTFS_DIR="binary"
 
 sudo cp -rf overlay-chuanda/* $TARGET_ROOTFS_DIR/
 
+# bt/wifi firmware
+sudo mkdir -p $TARGET_ROOTFS_DIR/system/lib/modules/
+sudo find ../../kernel/drivers/net/wireless/rockchip_wlan/*  -name "*.ko" | \
+    xargs -n1 -i sudo cp {} $TARGET_ROOTFS_DIR/system/lib/modules/
+
+
 finish() {
 	sudo umount $TARGET_ROOTFS_DIR/dev
 	exit -1
@@ -12,9 +18,23 @@ trap finish ERR
 
 sudo mount -o bind /dev $TARGET_ROOTFS_DIR/dev
 
+#------------------------------------------------------------------------------------------
 cat <<EOF | sudo chroot $TARGET_ROOTFS_DIR
 
 #--------- install base app ---------
+apt-get update
+
+#for bt udev
+chmod o+x /usr/lib/dbus-1.0/dbus-daemon-launch-helper
+echo exit 101 > /usr/sbin/policy-rc.d
+chmod +x /usr/sbin/policy-rc.d
+apt-get install -y blueman
+rm -f /usr/sbin/policy-rc.d
+rm /etc/xdg/autostart/blueman.desktop
+mv -f /etc/xdg/autostart/blueman.desktop.back /etc/xdg/autostart/blueman.desktop
+
+apt-get install -y at
+apt-get install -y bluez-hcidump
 
 
 # custom app
@@ -32,6 +52,6 @@ apt-get clean
 rm -rf /var/lib/apt/lists/*
 
 EOF
-
+#------------------------------------------------------------------------------------------
 
 sudo umount $TARGET_ROOTFS_DIR/dev
