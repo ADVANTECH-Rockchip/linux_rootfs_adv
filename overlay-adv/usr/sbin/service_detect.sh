6077c77102f9ee6cb9becfbaa5e600d8e64903ba
advyunjin <yunjin.jiang@advantech.com.cn>
2021-03-12 09:25:07 +0800

    add adv_service_detect for system recovery

---
+++ overlay-adv/usr/sbin/service_detect.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+sleep 10
+
+flag1=`systemctl list-units |grep systemd-logind |awk {'print $3'}`
+if [ x$flag1 != "xactive" ];then
+    echo "[ADV] systemd-logind boot fail" > /dev/ttyFIQ0
+fi
+
+flag2=`systemctl list-units |grep systemd-user-sessions |awk {'print $3'}`
+if [ x$flag2 != "xactive" ];then
+    echo "[ADV] systemd-user-sessions boot fail" > /dev/ttyFIQ0
+fi
+
+flag3=`systemctl list-units |grep lightdm |awk {'print $3'}`
+if [ x$flag3 != "xactive" ];then
+    echo "[ADV] lightdm boot fail" > /dev/ttyFIQ0
+fi
+
+flag4=`systemctl list-units |grep serial-getty@ttyFIQ0 |awk {'print $3'}`
+if [ x$flag4 != "xactive" ];then
+    echo "[ADV] serial-getty@ttyFIQ0 boot fail" > /dev/ttyFIQ0
+fi
+
+
+if [[ x$flag1 == "xactive" ]] && [[ x$flag2 == "xactive" ]] && [[ x$flag3 == "xactive" ]] && [[ x$flag4 == "xactive" ]];then
+    # disable WDT
+    echo "[ADV] service boot success" > /dev/ttyFIQ0
+    echo 1 > /sys/class/adv_timer_class/adv_timer_device/timer_flag
+else
+    echo "[ADV] service boot fail" > /dev/ttyFIQ0
+fi
+
+
